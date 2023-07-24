b21653f270b7bfe29e10122ac07e044631fcba4d
Alireza Rabie <alireza@aazerra.dev>
2023-07-24 09:54:33 +0330

    changed package name

--- src/utils/list.py
+++ src/utils/list.py
@@ -1,5 +1,11 @@
+from itertools import chain
+
+
 def chunks(arr, n=2):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(arr), n):
         yield arr[i:i + n]
 
+
+def flatten(arr):
+    return list(chain(*arr))
